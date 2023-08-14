dc68d8113aaf3178efdba3ffedbf373075f951cd
Ed Barberis <ed.barberis@gmail.com>
2023-08-14 11:30:36 -0400

    Upgraded Terraform to 1.5.5; AWS to 5.12; and EKS Module to 19.16.0.

--- builders/terraform/aws/cnao-lab/main.tf
+++ builders/terraform/aws/cnao-lab/main.tf
@@ -221,7 +221,7 @@ module "vm" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = ">= 19.15.4"
+  version = ">= 19.16.0"
 
   cluster_name    = local.cluster_name
   cluster_version = var.aws_eks_kubernetes_version
