038a1d610ac6465d901f9309503ac3b25e591cc7
Ed Barberis <ed.barberis@gmail.com>
2023-09-23 10:13:34 -0400

    Added support for Oracle Java SE JDK 21.

---
+++ provisioners/scripts/common/install_oracle_java_jdk_21.sh
@@ -0,0 +1,37 @@
+#!/bin/sh -eux
+# install java se 21 development kit by oracle.
+
+# install java se 21 development kit. --------------------------------------------------------------
+jdk_home="jdk21"
+jdk_build="21"
+jdk_folder="jdk-${jdk_build}"
+jdk_binary="${jdk_folder}_linux-x64_bin.tar.gz"
+jdk_sha256="36cd5e9661360a8fece4fd31cf678cd6611f3742633dab3b3244e037ba0b095f"
+
+# create java home parent folder.
+mkdir -p /usr/local/java
+cd /usr/local/java
+
+# download jdk 21 binary from oracle otn.
+wget --no-verbose https://download.oracle.com/java/${jdk_build:0:2}/archive/${jdk_binary}
+
+# verify the downloaded binary.
+echo "${jdk_sha256} ${jdk_binary}" | sha256sum --check
+# ${jdk_folder}_linux-x64_bin.tar.gz: OK
+
+# extract jdk 21 binary and create softlink to 'jdk21'.
+rm -f ${jdk_home}
+rm -rf ${jdk_folder}
+tar -zxvf ${jdk_binary} --no-same-owner --no-overwrite-dir
+chown -R root:root ./${jdk_folder}
+ln -s ${jdk_folder} ${jdk_home}
+rm -f ${jdk_binary}
+
+# set jdk 21 home environment variables.
+JAVA_HOME=/usr/local/java/${jdk_home}
+export JAVA_HOME
+PATH=${JAVA_HOME}/bin:$PATH
+export PATH
+
+# verify installation.
+java --version
