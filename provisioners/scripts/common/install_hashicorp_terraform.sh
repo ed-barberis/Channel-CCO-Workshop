106712dd2f1cc00bec4d0cf4ce965d2563610e76
Ed Barberis <ed.barberis@gmail.com>
2023-10-09 10:37:09 -0400

    Upgraded Terraform to 1.6.0; AWS to 5.20; and EKS Module to 19.17.1.

--- provisioners/scripts/common/install_hashicorp_terraform.sh
+++ provisioners/scripts/common/install_hashicorp_terraform.sh
@@ -28,9 +28,9 @@
 #---------------------------------------------------------------------------------------------------
 
 # install hashicorp terraform. ---------------------------------------------------------------------
-terraform_release="1.5.7"
+terraform_release="1.6.0"
 terraform_binary="terraform_${terraform_release}_linux_amd64.zip"
-terraform_sha256="c0ed7bc32ee52ae255af9982c8c88a7a4c610485cf1d55feeb037eab75fa082c"
+terraform_sha256="0ddc3f21786026e1f8522ba0f5c6ed27a3c8cc56bfac91e342c1f578f8af44a8"
 
 # create local bin directory (if needed).
 mkdir -p /usr/local/bin
