09da96d677ff0fd28745449fb49b871735f58a7d
Ed Barberis <ed.barberis@gmail.com>
2024-03-11 14:25:46 -0400

    Upgraded Terraform to 1.7.4; AWS to 5.40; and EKS Module to 20.8.2.

--- builders/terraform/aws/cco-lab/main.tf
+++ builders/terraform/aws/cco-lab/main.tf
@@ -221,7 +221,7 @@ module "vm" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = ">= 20.5.1"
+  version = ">= 20.8.2"
 
   cluster_name    = local.cluster_name
   cluster_version = var.aws_eks_kubernetes_version
