d9d66265a25265089cd9032a0c03f9dc8b5e71b2
Ed Barberis <ed.barberis@gmail.com>
2024-05-03 15:45:01 -0400

    Updated support for Docker 'v26.1.1'.

--- provisioners/scripts/common/install_user_env.sh
+++ provisioners/scripts/common/install_user_env.sh
@@ -7,7 +7,7 @@ user_group="${user_group:-}"
 user_home="${user_home:-}"
 user_docker_profile="${user_docker_profile:-false}"
 user_prompt_color="${user_prompt_color:-green}"
-d_completion_release="${d_completion_release:-26.1.0}"
+d_completion_release="${d_completion_release:-26.1.1}"
 
 # set default value for devops home environment variable if not set. -------------------------------
 devops_home="${devops_home:-/opt/cco-lab-devops}"
@@ -27,7 +27,7 @@ Usage:
                                                                 #            valid colors:
                                                                 #              'black', 'blue', 'cyan', 'green', 'magenta', 'red', 'white', 'yellow'
                                                                 #
-    [root]# export d_completion_release="26.1.0"                # [optional] docker completion for bash release (defaults to '26.1.0').
+    [root]# export d_completion_release="26.1.1"                # [optional] docker completion for bash release (defaults to '26.1.1').
     [root]# export devops_home="/opt/cco-lab-devops"            # [optional] devops home (defaults to '/opt/cco-lab-devops').
     [root]# $0
 EOF
